bcf84379c3c39c1e2bab0a679401284a4852bb6e
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-09-20 20:55:28 +0100

    Add support for 2d and all one shape

--- bingo.rb
+++ bingo.rb
@@ -6,3 +6,18 @@ get '/' do
   @shapes = @shapes.sort {|a,b| rand <=> rand}
   erb :index
 end
+
+get '/2d' do
+  @shapes = %w(square triangle rectangle circle hexagon pentagon)
+  @shapes = @shapes.sort {|a,b| rand <=> rand}
+  erb :index
+end
+
+get '/:shape' do
+  @shapes = Array.new(6,params[:shape])
+  erb :index
+end
+
+
+
+
